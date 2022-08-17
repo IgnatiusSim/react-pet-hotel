60addf1d5fab1d94046abc622606e16cf9605f6f
IgnatiusSimungu <71499403+IgnatiusSimungu@users.noreply.github.com>
2022-08-17 16:29:06 +0200

    Config changes

--- 1-Project/frontend/src/config/production.js
+++ 1-Project/frontend/src/config/production.js
@@ -1,4 +1,21 @@
-const firebaseConfig = <insert firebase config here>;
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDMIG3pLVyaC2eTA1UrETozSWKE2IxK3Uc",
+
+  authDomain: "pet-hotel-prod-82303.firebaseapp.com",
+
+  projectId: "pet-hotel-prod-82303",
+
+  storageBucket: "pet-hotel-prod-82303.appspot.com",
+
+  messagingSenderId: "903161783236",
+
+  appId: "1:903161783236:web:d2f7b25f22b619431cfa91",
+
+  measurementId: "G-48GYS6P8VQ"
+
+};
+
 
 // Cloud Functions
 const backendUrl = `https://us-central1-${
